269f9404d9198875be4e17126c267ec736d01674
Host-Info.net <50676042+aerobotpro@users.noreply.github.com>
2020-03-20 19:07:45 -0700

    Version 0.0.3

--- src/py/covid-19-utils/__init__.py
+++ src/py/covid-19-utils/__init__.py
@@ -1,5 +1,56 @@
 from requests import get
 import json
+from os import getcwd
+#NOT PROFICIENT FOR ASYNC! May crash sockets due to blocking.
+
+class getCdcWorldMap:
+    def __init__(self):
+        fname = "cdc_world_map.png"
+        url = "https://www.cdc.gov/coronavirus/2019-ncov/images/outbreak-coronavirus-world.png"
+        with open(fname, 'wb') as handle:
+            response = get(
+                url, stream=True
+                )
+            if not response.ok:
+                    raise "cdcWorldMap::Error::Failed to download image"
+                    fname = None
+            for block in response.iter_content(1024):
+                if not block:
+                    break
+                handle.write(block)
+        if fname is not None:
+            fst = f"{getcwd()}/{fname}"
+        else: fst = None
+        self.WorldMapLocalFile = fst
+        self.WorldMapLink = url
+        self.WorldMapHtml = f"""<img src="{self.WorldMapLink}">"""
+        
+
+class getCountryFlag:
+    # Gets BEST choice country, based on input and returns flag links.
+    def __init__(self, countryName, download=False):
+           
+        alpha_2 = get(f"https://restcountries.eu/rest/v2/name/{countryName}").json()[0]['alpha2Code']
+        self.flagHtml = f"""<img src="https://www.countryflags.io/{alpha_2}/shiny/64.png">"""
+        self.flagLink = f"https://www.countryflags.io/{alpha_2}/shiny/64.png"
+        
+        if download:
+            fname = f"{alpha_2}.png"
+            with open(fname, 'wb') as handle:
+                response = get(
+                    self.flagLink, stream=True
+                    )
+                if not response.ok:
+                        raise "getCountryFlag::Error::Failed to download image"
+                        fname = None
+                for block in response.iter_content(1024):
+                    if not block:
+                        break
+                    handle.write(block)
+            if fname is not None:
+                self.flagLocalFile = f"{getcwd()}/{fname}"
+            else: self.flagLocalFile = None             
+
 
 class covidUpdate:
     def __init__(self, BOOL_country_data=False):
@@ -30,5 +81,7 @@ class covidUpdate:
         self.totalRecovered = aj['recovered']
         self.timeUpdatedUnix = aj['updated']
         #
+        
         self.countryData = serializedCountryData
         # cases, todayCases, deaths, todayDeaths, recovered, active, critical, casesPerOneMillion
+
